bdc8b375b12d93089b25e33af31cbb414d1f4263
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-03-12 19:17:26 -0300

    start boilerplate

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500 text-center uppercase underline font-bold">
+        Hello World
+      </h1>
+    </div>
+  );
+};
+
+export default App;
