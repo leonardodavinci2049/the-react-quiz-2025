5a6e21f0f0d015df3810905fd1fc13d0dc45cb71
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-03-24 13:15:48 -0300

    project finishing

--- src/contexts/QuizProvider.tsx
+++ src/contexts/QuizProvider.tsx
@@ -100,7 +100,7 @@ function QuizProvider({ children }: { children: React.ReactNode }) {
     fetch('http://localhost:9000/questions')
       .then((res) => res.json())
       .then((data) => dispatch({ type: 'dataReceived', payload: data }))
-      .catch((err) => dispatch({ type: 'dataFailed' }));
+      .catch(() => dispatch({ type: 'dataFailed' }));
   }, []);
 
   return (
